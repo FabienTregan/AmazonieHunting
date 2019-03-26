0529b5c5f2c8a0db070a961b4682d1a44f2f0764
Fabien Tregan <fabien@treXXgan.fr>
2019-03-26 12:35:38 +0100

    Detects if the target is touched.

--- tir/aiming.js
+++ tir/aiming.js
@@ -3,7 +3,8 @@ function aiming() {
     var nbPlans = 5
     var position = { x: 0, y: 0 }
     var targetPosition = { x: 0, y: 0 }
-    var moveSpeed = { x: 0, y: 0 }
+
+    var arrowHittingPoint = { x: 924, y: 320 }
 
     var plansSpeed = [1.333, 1.143, 1, 0.889, 0.800]
 
@@ -18,7 +19,7 @@ function aiming() {
             var planId = "plan" + i
             var planNode = document.getElementById(planId)
 
-            var left = ( (1920 - 2880) / 2 - position.x * plansSpeed[i-1]) / 1920 * main.offsetWidth
+            var left = ((1920 - 2880) / 2 - position.x * plansSpeed[i - 1]) / 1920 * main.offsetWidth
             planNode.style.left = left + "px"
         }
     }
@@ -28,17 +29,24 @@ function aiming() {
         var animal = document.getElementById("animal")
         var main = document.getElementById("main")
 
-        var left = (currentAnimal.position.x - position.x * plansSpeed[currentAnimal.position.z - 1]) / 1920 * main.offsetWidth
-        var top = (currentAnimal.position.y) / 1080 * 100
-        animal.style.left = left + "px"
-        animal.style.top = top + "%"
+        var animalCoords = scaleToActualScreenSize(animalFHDCoordinates())
 
-        var width = animal.naturalWidth / 1920 * 100
-        var height = animal.naturalHeight / 1080 * 100
-        animal.style.height = height + "%"
-        animal.style.width = width + "%"
+        animal.style.left = animalCoords.x + "px"
+        animal.style.top = animalCoords.y + "px"
+
+        var animalSize = scaleToActualScreenSize({ x: animal.naturalWidth, y: animal.naturalHeight })
+        animal.style.width = animalSize.x + "px"
+        animal.style.height = animalSize.y + "px"
 
         animal.style.zIndex = planIndexToZIndex(currentAnimal.position.z)
+    }
+
+    function scaleToActualScreenSize(coords) {
+        var main = document.getElementById("main")
+        return {
+            x: coords.x / 1920 * main.offsetWidth
+            , y: coords.y / 1080 * main.offsetHeight
+        }
     }
 
     function planIndexToZIndex(n) {
@@ -53,26 +61,82 @@ function aiming() {
         if (position.y != targetPosition.y) {
             position.y = position.y * 0.95 + targetPosition.y * 0.05
         }
-
     }
 
     function updateBow() {
         var main = document.getElementById("main")
-
-        var bottom = - position.y / 1080 * 100
-        var left = position.x / 1920 * main.offsetWidth
-
         var bowNode = document.getElementById("bow")
-        bowNode.style.bottom = bottom + "%"
+        var bow0ImageNode = document.querySelector("#bow .bow0")
+
+        var top = (position.y + 1080 - bow0ImageNode.naturalHeight) / 1080 * main.offsetHeight
+        var left = (position.x - (bow0ImageNode.naturalWidth - 1920) / 2) / 1920 * main.offsetWidth
+
+        bowNode.style.top = top + "px"
         bowNode.style.left = left + "px"
 
         var arrowNode = document.getElementById("arrow")
-        arrowNode.style.bottom = bottom + "%"
+        arrowNode.style.top = top + "px"
         arrowNode.style.left = left + "px"
+    }
+
+    function updateVisee() {
+
+        var bow0ImageNode = document.querySelector("#bow .bow0")
+
+        var impact = {
+            x: (position.x - (bow0ImageNode.naturalWidth - 1920) / 2 + arrowHittingPoint.x)
+            , y: (position.y + arrowHittingPoint.y)
+        }
+
+        var animalCoords = animalFHDCoordinates()
+        var animalImg = document.getElementById("animal")
+        var isImpactInAnimalRectangle =
+            (impact.x > animalCoords.x)
+            && (impact.x < animalCoords.x + animalImg.naturalWidth)
+            && (impact.y > animalCoords.y)
+            && (impact.y < animalCoords.y + animalImg.naturalHeight)
 
         // REMOVE
-        viseur.style.left = (1920/2 + position.x) / 1920 * main.offsetWidth
-        viseur.style.top = position.y / 1080 * main.offsetHeight
+        if (isImpactInAnimalRectangle) {
+            var impactInAnimalImage = {
+                x: impact.x - animalCoords.x
+                , y: impact.y - animalCoords.y
+            }
+            if (isPixelOpac(animalImg, impactInAnimalImage)) {
+                viseur.style.borderColor = "red"
+            } else {
+                viseur.style.borderColor = "orange"
+            }
+        } else {
+            viseur.style.borderColor = "lime"
+        }
+
+        var impactScreenCoords = scaleToActualScreenSize(impact)
+        viseur.style.left = impactScreenCoords.x
+        viseur.style.top = impactScreenCoords.y
+    }
+
+    function isPixelOpac(img, coords) {
+        var canvas = document.createElement('canvas');
+        canvas.width = 1
+        canvas.height = 1
+        var context2d = canvas.getContext('2d')
+        context2d.drawImage(img, coords.x, coords.y, 1, 1, 0, 0, 1, 1)
+
+        var pixelData = context2d.getImageData(0, 0, 1, 1).data
+        var transparency = pixelData[3]
+
+        show.innerHTML = JSON.stringify(transparency)
+
+        return transparency > 196
+    }
+
+    function animalFHDCoordinates() {
+        return {
+            x: (currentAnimal.position.x - position.x * plansSpeed[currentAnimal.position.z - 1])
+            , y: (currentAnimal.position.y)
+
+        }
     }
 
     function updateScreen() {
@@ -80,24 +144,23 @@ function aiming() {
         updatePosition()
         updatePlans()
         updateBow()
+        updateVisee()
     }
 
     function mouseMoved(coordinates) {
         var canvasHeight = canvas.offsetHeight
         var canvasWidth = canvas.offsetWidth
-    
+
         targetPosition = {
             x: (coordinates.x / canvasWidth * 2 - 1) * 400
             , y: (coordinates.y / canvasHeight * 2 - 1) * 400
         }
 
-        show.innerHTML = JSON.stringify(targetPosition)
-        
         targetPosition.x = Math.min(targetPosition.x, MAX_POSITION.x)
         targetPosition.x = Math.max(targetPosition.x, -MAX_POSITION.x)
-        
+
         targetPosition.y = Math.min(targetPosition.y, MAX_POSITION.y)
-        targetPosition.y = Math.max(targetPosition.y, 0)
+        targetPosition.y = Math.max(targetPosition.y, 97)
 
     }
 
--- tir/shooting.js
+++ tir/shooting.js
@@ -1,26 +1,56 @@
 function shooting() {
+    const ARROW_FRAME_DURATION = 100
     var arrow = document.getElementById("arrow")
     var bow = document.getElementById("bow")
+    var startTimeMs
 
     function updateScreen() {
+        var animationFrameNumber = Math.floor(1 + (Date.now() - startTimeMs) / ARROW_FRAME_DURATION)
+
+        if (animationFrameNumber > 8) {
+            animationFrameNumber = 0
+            state("aiming")
+        }
+
+        showAnimationFrame(animationFrameNumber)
+    }
+
+    function showAnimationFrame(n) {
+        arrow.classList.remove("arrow0", "arrow1", "arrow2", "arrow3", "arrow4", "arrow5", "arrow6")
+        arrow.classList.add(arrowAnimationFrameClass(n))
+
+        bow.classList.remove("bow0", "bow1", "bow2", "bow3")
+        bow.classList.add(bowAnimationFrameClass(n))
+    }
+
+    function arrowAnimationFrameClass(frameNumber) {
+        return "arrow" + frameNumber
+    }
+
+    function bowAnimationFrameClass(frameNumber) {
+        var conversion = {
+            0: 0
+            , 1: 1
+            , 2: 1
+            , 3: 2
+        }
+
+        var n = (frameNumber > 3) ? 3 : conversion[frameNumber]
+
+        return "bow" + n
     }
 
     function start() {
-        //restart the animation
-        var src = arrow.src
-        arrow.src = src
+        startTimeMs = Date.now()
 
-        arrow.style.opacity = 1
-        bow.style.opacity = 0
-        
-        var animalId = getParameter("animal")
-        window.setTimeout(function() {window.location.href = "../congratulation?animal=" + animalId}, 1500)
+        // var animalId = getParameter("animal")
+        // window.setTimeout(function() {window.location.href = "../congratulation?animal=" + animalId}, 1500)
     }
 
     return {
         updateScreen: updateScreen
-        , mouseMoved: function() {}
-        , clicked: function () {}
+        , mouseMoved: function () { }
+        , clicked: function () { }
         , start: start
     }
 }
