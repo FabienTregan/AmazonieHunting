62afbd644ee29cc9590e313ff16c6f484565f64e
Fabien Tregan <fabien@treXXgan.fr>
2019-04-16 10:51:09 +0200

    Adds configuration file and allows to hide the mouse.

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+
+    // if set to 'true', will hide the mouse pointer (usefull on touchscreens)
+    hideMouse: true,
+
+    // time in seconds before the screensaver appears. If 0, screensaver is disabled.
+    screensaverDelay : 120,
+}
