9b385daefc38542a4fe8738f3e0fdfe181cbc909
Fabien Tregan <treguy@free.fr>
2019-06-04 15:42:00 +0200

    Goes fullscreen after intro screen.

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {
 
     // if set to 'true', will hide the mouse pointer (usefull on touchscreens)
-    hideMouse: true,
+    hideMouse: false,
 
     // time in seconds before the screensaver appears. If 0, screensaver is disabled.
-    screensaverDelay : 120,
+    screensaverDelay : 0,
 }
